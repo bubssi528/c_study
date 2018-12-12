f22e80c17c92468d796d53830a98112f4fd264e9
bubssi528 <45654555+bubssi528@users.noreply.github.com>
2018-12-12 19:39:21 +0900

    Code_modify
    code error debug and changing

--- String02/String02/pw_check2.c
+++ String02/String02/pw_check2.c
@@ -4,24 +4,27 @@
 
 int main() {
 	char pass[] = "1234";
-	char answer[100];
-	int buffer = 0;
+	char answer[5];
 	int i = 0;
+
 	printf("비밀번호를 입력하시오 : ");
-	while (1) {
+	for (;;) {
 		answer[i] = getch();
-		if (answer[i] == '\0') {
+		if (answer[i] == 13)
+			break;
+		if (answer[i] == NULL) {
 			i--;
 			continue;
 		}
 		putch('*');
-		if (answer[i] == '\n')
-			break;
+		i++;
 	}
-	answer[i - 1] = NULL;
+	answer[i] = NULL;
+
 	if (strcmp(pass, answer) == 0)
 		printf("\n비밀번호 일치\n");
 	else
 		printf("\n비밀번호 불일치\n");
 	return 0;
 }
+
